59fc3bdf657d16d3b4c046d3d8b888a531c8ea6d
fs-coder <fs.vaishnavimane@gmail.com>
2022-06-03 18:59:57 +0530

    food inquiry

--- testt.py
+++ testt.py
@@ -1,4 +1,9 @@
-print('Hi, there')
-print("it's me")
-print("Vaishnavi")
-print ("hello!")
+food = input("Are you starving?")
+if food == 'yes'.lower():
+	food_type = input("what would you like to fill in your tummy? bitterly healthy or deliciously unhealthy?")
+	if food_type == 'bitterly healthy'.lower():
+		print("Being healthy is Important")
+	else:
+		print("Your tummy will gonna cry!")
+else:
+	print("You must eat something")
